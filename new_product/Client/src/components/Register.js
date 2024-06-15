43cb833aaf0a2e3c29a3d6df6f54c8ba5243bea7
Charanrajueluri123 <110445386+Charanrajueluri123@users.noreply.github.com>
2024-06-15 22:25:07 +0530

    Update Register.js

--- new_product/Client/src/components/Register.js
+++ new_product/Client/src/components/Register.js
@@ -13,6 +13,8 @@ function Signup() {
   const [passwordMessage, setPasswordMessage] = useState({ text: "", isError: false });
   const navigate = useNavigate();
 
+  axios.defaults.withCredentials = true;
+
   const validatePassword = (password) => {
     const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return regex.test(password);
