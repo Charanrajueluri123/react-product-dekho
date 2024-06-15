0a2ac5f1430ef435d1fae6db28ce922cd0e4701f
Charanrajueluri123 <110445386+Charanrajueluri123@users.noreply.github.com>
2024-06-15 22:20:04 +0530

    Update index.js

--- new_product/server/index.js
+++ new_product/server/index.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const cors = require("cors");
 const UsersModel = require('./models/Users');
 
-const app = express();
-
-app.use(cors());
+const app = express();  
 
 app.use(cors(
   {
