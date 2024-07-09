91551eaea7e7b06d9d92063fff8cfe5ed412a74b
Charanrajueluri123 <110445386+Charanrajueluri123@users.noreply.github.com>
2024-07-09 15:29:16 +0530

    Update SocialFollow.js

--- new_product/Client/src/components/SocialFollow.js
+++ new_product/Client/src/components/SocialFollow.js
@@ -11,22 +11,22 @@ import {
   return (
     <div class="social-container">
       <p style={{color:"white",fontSize:"20px",marginLeft:"50px"}}>Social Follow</p>
-      <a href="https://www.youtube.com/c/jamesqquick"
+      <a href="https://www.linkedin.com/in/eluri-charan-raju-452413247/"
         className="linkedin">
         <FontAwesomeIcon icon={faLinkedin} size="2x" />
       </a>
-      <a href="https://www.youtube.com/c/jamesqquick"
+      <a href="https://www.youtube.com/"
         className="youtube social">
         <FontAwesomeIcon icon={faYoutube} size="2x" />
       </a>
-      <a href="https://www.facebook.com/learnbuildteach/"
+      <a href="https://www.facebook.com"
         className="facebook social">
         <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
-      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+      <a href="https://x.com/Cherry31448" className="twitter social">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
-      <a href="https://www.instagram.com/learnbuildteach"
+      <a href="https://www.instagram.com/charanraju_123/"
         className="instagram social">
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
